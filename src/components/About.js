6dbaa0ce1ad1c7f1e22f23ab3fb2597f32b1ed9e
Harold <harold@pop-os.localdomain>
2022-09-24 18:40:52 -0400

    Added Lightning Widget

--- src/components/About.js
+++ src/components/About.js
@@ -4,17 +4,25 @@ import doge from '../images/doge.webp';
 import web3 from '../images/web3.webp';
 import virtual from '../images/virtual.webp';
 import cyber from '../images/cyber.webp';
+import {Helmet} from "react-helmet";
+
+
 class About extends Component {
+
     render() {
         return (
             
             <section class = "about-section" id="about">
             <div class='section-title'>About Me</div>
+            
+            <Helmet>
+                <script src="https://embed.twentyuno.net/js/app.js"></script>
+            </Helmet>
 
             <div class = "about-content">
                 <div class='image-and-description'>
                     <div class = "about-image-container">
-                        <img class= 'about-image' src={harold} width='100%'/>
+                        <lightning-widget name="Harold Than" accent="#ff8000" to="harold@getalby.com" image="https://scontent-bos5-1.xx.fbcdn.net/v/t39.30808-6/298841264_802905871121966_6082265023474547667_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=5bOMyXua_twAX8fozej&_nc_ht=scontent-bos5-1.xx&oh=00_AT_GtoOORzsXpp3C9bW9pkRAso7eJ_eceJdfuOn3F8BuDA&oe=63351479" />
                     </div>
                     <div class = 'about-description-and-resume'>
                         <div class ="tiles" >
